26b375b31e205631abe9f6d52d4c232fcbbb7773
Victor Tsai <princeoftennis735@gmail.com>
2020-05-16 12:24:54 +0800

    補上該會員所有筆記本及讀取筆記本資訊(依照bookid) 補上取得會員資訊邏輯

--- src/main/java/victor/notebook/controller/NotebookController.java
+++ src/main/java/victor/notebook/controller/NotebookController.java
@@ -1,5 +1,6 @@
 package victor.notebook.controller;
 
+import java.util.List;
 import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import victor.notebook.annotation.SessionFilter;
 import victor.notebook.domain.Member;
+import victor.notebook.domain.Notebook;
+import victor.notebook.service.NoteBookService;
 import victor.notebook.util.SystemConstant;
 
 @Controller
@@ -21,25 +25,28 @@ public class NotebookController {
 	
 	@Autowired
 	private SystemConstant systemConstant;
+	@Autowired
+	private NoteBookService notebookService;
 	
 	@RequestMapping(value="/list",method= {RequestMethod.GET,RequestMethod.POST})
 	@SessionFilter
 	public String listNotebook(HttpServletRequest req,Model model) {
 		// check member in session
 		Member recentUser = (Member)req.getSession().getAttribute(systemConstant.CurrentUser);
-		if(Objects.nonNull(recentUser)) {
-			
-		}
-		// if not in session
-		else {
-			
-		}
-		// if in session
+//		if(Objects.nonNull(recentUser)) {
+//			
+//		}
+//		// if not in session
+//		else {
+//			
+//		}
+		List<Notebook> notebooks = notebookService.listNotebookByMemberId(recentUser.getId());
+		model.addAttribute("notebook", notebooks);
 		return "fia.Notebooks";
 	}
 	
 	@RequestMapping(value="/edit/{noteId}",method= {RequestMethod.GET,RequestMethod.POST})
-	@SessionFilter
+//	@SessionFilter
 	public String addoreditnotebook(HttpServletRequest req,Model model,@PathVariable("noteId") int id) {
 		return "fia.NotebookInfo";
 	}
@@ -51,4 +58,10 @@ public class NotebookController {
 		return "";
 	}
 	
+	@RequestMapping(value="/SaveOrUpdate",method= {RequestMethod.POST},produces="application/json; charset=utf-8")
+	@ResponseBody
+	public String saveOrUpdate(HttpServletRequest req,Model model) {
+		notebookService.saveOrupdate();
+		return "" ;
+	}
 }
